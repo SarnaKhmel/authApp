c3fa09cd924353ec2fc83e48009085131d32bff1
SarnaKhmel <olsarnat@gmail.com>
2020-11-20 11:36:36 +0200

    SignIn/Up + Timer page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
+import fire from './fire';
+import Login from "./Login"
+import Timer from './Timer'
 
 function App() {
   const [user, setUser] = useState('');
@@ -7,18 +10,92 @@ function App() {
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-  const [hasAcount, setHasAccount] = useState('');
+  const [hasAccount, setHasAccount] = useState('');
   const [descTime, setDescTime] = useState('');
   const [mobileTime, setMobileTime] = useState('');
 
-  const handleLogin = () => {
-
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
   }
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  const handleLogin = () => {
+    clearErrors();
+      fire
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch((err) => {
+          switch(err.code) {
+            case "auth/invalid-email":
+            case "auth/user-disabled":
+            case "auth/user-not-found":
+              setEmailError(err.message);
+              break;
+            case "auth/wrong-password":
+              setPasswordError(err.message);
+              break;
+          }
+        });
+  };
+
+  const handleSingup = () => {
+    clearErrors();
+      fire
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch((err) => {
+          switch(err.code) {
+            case "auth/email-already-in-use ":
+            case "auth/invalid-email":
+              setEmailError(err.message);
+              break;
+            case "auth/weak-password":
+              setPasswordError(err.message);
+              break;
+          }
+      });
+  }
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  }
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged( user => {
+      if(user) {
+        clearInputs();
+         setUser(user);
+      }
+      else {
+        setUser("");
+      }
+    })
+  }
+
+  useEffect (() => {
+    authListener();
+  }, [])
 
 
   return (
     <div className="App">
-     
+     <Login 
+          email={email}
+          setEmail={setEmail}
+          password={password} 
+          setPassword={setPassword} 
+          handleLogin={handleLogin} 
+          handleSingup={handleSingup}
+          hasAccount={hasAccount}
+          setHasAccount={setHasAccount}
+          emailError={emailError}
+          passwordError={passwordError}
+          />
     </div>
   );
 }
---
+++ src/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Timer = () => {
+
+    return (
+        <section className='timer'>
+        
+        </section>
+    )
+}
+
+ export default Timer
